f4b6f8b6e9b78c4d47cee420d31df3fc3afdd4eb
Alex Hernandez <alexhernandez@wireless-10-146-45-22.public.utexas.edu>
2021-10-30 20:08:41 -0500

    fixing progress bar

--- progress_bar.js
+++ progress_bar.js
@@ -1,38 +1,17 @@
-var likeCounter = 0;
-var dislikeCounter = 0;
+let likeCount = 0;
+let dislikeCount = 0;
 
 
 function incrementLike() {
-    likeCounter++
-    bar_percent = document.getElementById("my_completion_bar").style.width;
-    bar_width = "";
-    
-    for (let i=0; i < (bar_percent.length - 1); i++) {
-        bar_width = bar_percent[i];
+    likeCount++;
+    if (likeCount >= 5 && dislikeCount >= 5) {
+        window.location.href = "result_page.html"
     }
-    if (parseInt(bar_width) < 100){
-        incrementProgressBar(bar_width)
-    }
-    console.log(likeCounter)
 }
 
 function incrementDislike() {
-    dislikeCounter++
-    console.log(dislikeCounter)
-}
-
-function incrementProgressBar(bar_width) {
-    bar_percent = document.getElementById("my_completion_bar").style.width;
-
-    if (bar_percent != "1%") {
-        new_bar_width = 10 + parseInt(bar_width);
-        document.getElementById("my_completion_bar").style.width = "new_bar_width" + "%";
+    dislikeCount++;
+    if (likeCount >= 5 && dislikeCount >= 5) {
+        window.location.href = "result_page.html"
     }
-    else {
-        document.getElementById("my_completion_bar").style.width = "10%";
-    }
-    console.log(bar_width)
-   
-    console.log(bar_percent)
-
 }
